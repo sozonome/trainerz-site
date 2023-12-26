aec7791cffca6f8b3440f3efd7db7116327cb354
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-12-27 00:48:32 +0700

    chore: configure biome

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
+export default ({
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+} satisfies Config);
