0c8b1d94c34ec25a6cbe074e627fa2cbba59605e
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-12-31 15:17:32 +0700

    feat: add root layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-export default ({
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-} satisfies Config);
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      display: ['Castoro', ...fontFamily.sans],
+      body: ['Lato', ...fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
